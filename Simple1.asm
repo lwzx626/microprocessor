b52fe268bfd42d12aa4e824de0dc523faee82409
ss12516 <ss12516@skbl-422-012.ph.ic.ac.uk>
2019-01-28 14:39:44 +0000

    Keyboard

--- Simple1.asm
+++ Simple1.asm
@@ -1,59 +1,30 @@
 	#include p18f87k22.inc
 
 	extern	UART_Setup, UART_Transmit_Message  ; external UART subroutines
-	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
-	
-acs0	udata_acs   ; reserve data space in access ram
-counter	    res 1   ; reserve one byte for a counter variable
-delay_count res 1   ; reserve one byte for counter in the delay routine
+	extern  LCD_Setup, LCD_Write_Message, LCD_delay_ms	    ; external LCD subroutines
 
-tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
-myArray res 0x80    ; reserve 128 bytes for message data
-
-rst	code	0    ; reset vector
+	code
+	org 0x0
+	banksel PADCFG1
 	goto	setup
 
-pdata	code    ; a section of programme memory for storing data
-	; ******* myTable, data in programme memory, and its length *****
-myTable data	    "Hello World!\n"	; message, plus carriage return
-	constant    myTable_l=.13	; length of data
-	
-main	code
-	; ******* Programme FLASH read Setup Code ***********************
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup LCD
-	goto	start
-	
-	; ******* Main programme ****************************************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
-		
-	movlw	myTable_l-1	; output message to LCD (leave out "\n")
-	lfsr	FSR2, myArray
-	call	LCD_Write_Message
-	
-	movlw	myTable_l	; output message to UART
-	lfsr	FSR2, myArray
-	call	UART_Transmit_Message
+setup	bsf PADCFG1, REPU, BANKED   ;Set the pull-ups to on for PORTE
+	goto start
 
-	goto	$		; goto current line in code
-
-	; a delay subroutine if you need one, times around loop in delay_count
-delay	decfsz	delay_count	; decrement until zero
-	bra delay
-	return
+start	clrf LATE		    ;Write 0s to the LATE register 
+	movlw 0x0F
+	movwf TRISE		    ;PORTE 4-7 outputs and PORTE 0-3 inputs
+	movlw 0x00
+	movwf TRISD		    ;PORTD all outputs
+	movlw .10
+	call LCD_delay_ms
+	movff PORTE, PORTD	    ;output column to PORTD
+	movlw 0xF0
+	movwf TRISE		    ;PORTE 0-3 outputs and PORTE 4-7 inputs
+	movlw .10
+	call LCD_delay_ms
+	movf PORTE, 0		    ;output row to W
+	addwf PORTD		    ;add W to column and output to PORTD 
+	goto start
 
 	end
