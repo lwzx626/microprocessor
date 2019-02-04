21a23b60b9355206458ae0f9de8cd1fe9508c6c6
ss12516 <ss12516@skbl-422-012.ph.ic.ac.uk>
2019-02-04 17:02:49 +0000

    Digital to Analog Converters, Timers and Interrupts

--- Simple1.asm
+++ Simple1.asm
@@ -2,21 +2,62 @@
 
 rst	code	0x0000	; reset vector
 	goto	start
-	
+
+acs0    udata_acs   ; named variables in access ram
+LCD_cnt_l   res 1   ; reserve 1 byte for variable LCD_cnt_l
+LCD_cnt_h   res 1   ; reserve 1 byte for variable LCD_cnt_h
+LCD_cnt_ms  res 1   ; reserve 1 byte for ms counter
+
 int_hi	code	0x0008	; high vector, no low vector
 	btfss	INTCON,TMR0IF	; check that this is timer0 interrupt
 	retfie	FAST		; if not then return
-	incf	LATD		; increment PORTD
+	incf	LATE		; increment PORTE
+	nop
+	nop
+	clrf	PORTD
+	movlw	0x01	
+	nop
+	nop
+	movwf	PORTD
 	bcf	INTCON,TMR0IF	; clear interrupt flag
 	retfie	FAST		; fast return from interrupt
 
 main	code
-start	clrf	TRISD		; Set PORTD as all outputs
-	clrf	LATD		; Clear PORTD outputs
-	movlw	b'10000111'	; Set timer0 to 16-bit, Fosc/4/256
-	movwf	T0CON		; = 62.5KHz clock rate, approx 1sec rollover
+start	clrf	TRISE		; Set PORTE as all outputs
+	clrf	TRISD
+	clrf	LATE		; Clear PORTE outputs
+	movlw	b'11000001'	; Set timer0 to 16-bit, Fosc/4/4
+	movwf	T0CON		; = 4MHz clock rate, approx 1sec rollover
 	bsf	INTCON,TMR0IE	; Enable timer0 interrupt
 	bsf	INTCON,GIE	; Enable all interrupts
 	goto	$		; Sit in infinite loop
+
+
+; ** a few delay routines below here as LCD timing can be quite critical ****
+LCD_delay_ms		    ; delay given in ms in W
+	movwf	LCD_cnt_ms
+lcdlp2	movlw	.250	    ; 1 ms delay
+	call	LCD_delay_x4us	
+	decfsz	LCD_cnt_ms
+	bra	lcdlp2
+	return
+    
+LCD_delay_x4us		    ; delay given in chunks of 4 microsecond in W
+	movwf	LCD_cnt_l   ; now need to multiply by 16
+	swapf   LCD_cnt_l,F ; swap nibbles
+	movlw	0x0f	    
+	andwf	LCD_cnt_l,W ; move low nibble to W
+	movwf	LCD_cnt_h   ; then to LCD_cnt_h
+	movlw	0xf0	    
+	andwf	LCD_cnt_l,F ; keep high nibble in LCD_cnt_l
+	call	LCD_delay
+	return
+
+LCD_delay			; delay routine	4 instruction loop == 250ns	    
+	movlw 	0x00		; W=0
+lcdlp1	decf 	LCD_cnt_l,F	; no carry when 0x00 -> 0xff
+	subwfb 	LCD_cnt_h,F	; no carry when 0x00 -> 0xff
+	bc 	lcdlp1		; carry, then loop again
+	return			; carry reset so return
 
 	end
