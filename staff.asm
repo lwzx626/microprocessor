718a31d465e816284bd70e1870597e0ac3819037
SS12516 <SS12516@skbl-422-012.ph.ic.ac.uk>
2019-02-25 17:06:39 +0000

    haven't finished, test tues

--- staff.asm
+++ staff.asm
@@ -4,7 +4,10 @@
 	extern	LCD_Write_Hex			    ; external LCD subroutines
 	extern  ADC_Setup, ADC_Read		    ; external ADC routines
 	extern	LCD_delay_ms
-	
+	extern  position_check, motor_setup, motor_start, motor_for, motor_back
+	extern 	motor_stop, pluck_motor,left_shift_high, right_shift_low
+	extern	note_A, note_B, note_C, note_D, note_E, note_F, note_G, 
+	extern  note_#A, note_#B, note_#C, note_#D, note_#E, note_#F, note_#G
 
 acs0	udata_acs   ; reserve data space in access ram
 counter	    res 1   ; reserve one byte for a counter variable
@@ -14,8 +17,9 @@ postmp	    res 1
 ifplucked   res 1
    
 rst	code	0    ; reset vector
-	goto	setup
-
+	goto	setup	
+main	code
+	
 setup	bcf	EECON1, CFGS	; point to Flash program memory  
 	bsf	EECON1, EEPGD 	; access Flash program memory
 	call	LCD_Setup	; setup LCD
